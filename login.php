1ed8bd7c8bc20ab62fdce2a776b8dba13207cfec
Younes Ameziane <73488317+YounesAmeziane@users.noreply.github.com>
2021-07-14 11:55:22 +0100

    Create login.php

---
+++ login.php
@@ -0,0 +1,49 @@
+<?php session_start(); //doesn t workkkkkk :(?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-site-verification" content="ubzn0wDgOWLuIR7k1JWWAVuiF1OP0KVfmHiyVIK9B0g" />
+    <link rel="stylesheet" href="css/style-login.css">
+    <link rel="shortcut icon" href="img/0007.png">
+    <title>Leaxiano | Log in</title>
+</head>
+<script>
+
+</script>
+<?php
+  
+  //here php code
+  
+?>
+<body>
+    <div class="log_in_form">
+        <h1>Log in</h1>
+        <form action="" method="post">
+            <div class="row">
+                <input type="text" name="username" placeholder="Username" autocomplete="off">
+            </div>
+            <div class="row">
+                <input type="password" name="password" placeholder="Password" autocomplete="off">
+            </div>
+            <center>
+                <a href="">Forgot your password?</a><br>
+            </center>
+            <div class="row">
+                <input type="submit" name="submit" value="Log In">
+            </div>
+            <center>
+                <a href="register.php">Not enregistred yet?</a>
+            </center>
+        </form>
+    </div>
+    <div class="bottom">
+        <center>
+          <span class="credit">Created By Leax</a> | </span>
+          <span class="far fa-copyright"></span><a href="" class="rr"> 2021 All rights reserved.</a>
+        </center>
+    </div>
+</body>
+</html>
